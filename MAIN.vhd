296367dce6fca31f35dfb9c2d5c88f51f313c62c
lucastavarex <81393330+lucastavarex@users.noreply.github.com>
2022-07-12 18:05:54 -0300

    Refatoração de código

--- MAIN.vhd
+++ MAIN.vhd
@@ -7,13 +7,15 @@
 
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
 
 -- NESTE ARQUIVO PRINCIPAL(MAIN), SERÁ ARQUITETADO A 
 -- COMUNICAÇÃO DO SOFTWARE COM O HARDWARE(PLACA), DEFININDO AS ENTRADAS E SAÍDAS.
 
 ENTITY MAIN IS -- COMPONENTES DA FPGA
     PORT( 
-		V_SW: IN STD_LOGIC_VECTOR (17 DOWNTO 7);  -- VETOR DAS ENTRADAS 
+		V_SW: IN STD_LOGIC_VECTOR (2 DOWNTO 0);  -- VETOR DAS ENTRADAS 
+		CLOCK_50: in STD_LOGIC;
 	    LEDR: OUT STD_LOGIC_VECTOR (7 DOWNTO 0)  --  LED DAS FLAGS + LED DO RESULTADO  |                                                                            
 		  --                                                                           | 
 		  --                           ATENÇÃO!!!                    <-----------------*  
@@ -27,7 +29,9 @@ ARCHITECTURE DADOS OF MAIN IS
 -- SIGNAL SAIDA TEMPORÁRIO : STD_LOGIC_VECTOR(3 DOWNTO 0);
 SIGNAL RES : STD_LOGIC_VECTOR(3 DOWNTO 0); 
 -- SIGNAL SAIDA FINAL : STD_LOGIC_VECTOR(3 DOWNTO 0);
-SIGNAL RES2: STD_LOGIC_VECTOR (3 DOWNTO 0);
+SIGNAL RES2: STD_LOGIC_VECTOR (3 DOWNTO 0); 
+ 
+			
 
 
 COMPONENT ULA IS 
@@ -43,11 +47,51 @@ COMPONENT ULA IS
             );
 END COMPONENT ULA;
 
+signal A: STD_LOGIC_VECTOR(3 downto 0) := "0000";
+	signal B: STD_LOGIC_VECTOR(3 downto 0) := "0000";
+	
+	--Counter Vector
+	signal counter: unsigned(7 downto 0) := "00000000";
+
+-- Dos 8 bits gerados por esse contador, os 4 menos significativos compoem A e os 4 mais significativos, B.
+-- Como clock recebido é de 50 Mhz, esta frequência foi dividida por 80 milhões, o que gera
+-- um período de contagem da ordem de 1,6 segundos.
+
+-- OBS! O contador não funcionou quando a frequencia foi dividida por 100 milhões para gerar um período 
+-- contagem de 2 segundos!
 
 BEGIN 
+    A(0) <= counter(0);
+	A(1) <= counter(1);
+	A(2) <= counter(2);
+	A(3) <= counter(3);
+	B(0) <= counter(4);
+	B(1) <= counter(5);
+	B(2) <= counter(6);
+	B(3) <= counter(7);
+	
+	
 
 -- AQUI ASSOCIA-SE AS SAIDAS E ENTRADAS DA NOSSA UNIDADE LÓGICA ARITMÉTICA COM A FPGA !!!
-UNIDADE: ULA PORT MAP (V_SW(17 DOWNTO 14),V_SW(13 DOWNTO 10),V_SW(9 DOWNTO 7),RES,LEDR(3),LEDR(1),LEDR(2),LEDR(0),RES2,LEDR(7),LEDR(6),LEDR(5) ,LEDR(4));
-    
 
-END DADOS;
+UNIDADE: ULA PORT MAP (A,B,V_SW( 2 DOWNTO 0),RES,LEDR(3),LEDR(1),LEDR(2),LEDR(0),RES2,LEDR(7),LEDR(6),LEDR(5) ,LEDR(4));
+
+
+process(CLOCK_50)
+	
+	 VARIABLE reductClock : integer range 100000000 downto 0 := 0;
+	 
+	begin
+	
+	    if (CLOCK_50'event and CLOCK_50='1') then
+	        if (reductClock <= 80000000) then
+	            reductClock := reductClock + 1;
+	       else
+	        counter <= counter + 1;
+	        reductClock := 0;
+	        
+	        end if;
+	    end if;
+	end process;
+	
+	end DADOS;
